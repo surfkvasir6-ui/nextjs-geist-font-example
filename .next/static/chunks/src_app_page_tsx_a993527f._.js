c7ab6178a4a7bb6a978abc72bfd4abe7e77d3fea
surfkvasir6-ui <surfkvasir6@gmail.com>
2025-08-08 16:30:46 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1754681446297

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a453ac92._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_ed5d7820._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_c4413217._.js",
+  "static/chunks/node_modules_168310da._.js"
+],
+    source: "dynamic"
+});
