0f39ca6130bf6c9146335dbb31c7c99c2f6050df
surfkvasir6-ui <surfkvasir6@gmail.com>
2025-08-28 16:07:05 -0300

    Add .next/static/chunks/src_app_page_tsx_cb5f1d10._.js 1756408024607

---
+++ .next/static/chunks/src_app_page_tsx_cb5f1d10._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_e830d924._.js",
+  "static/chunks/node_modules_@fortawesome_fontawesome-svg-core_ac4b76ed._.js",
+  "static/chunks/node_modules_@fortawesome_free-solid-svg-icons_index_mjs_6d4e8d45._.js",
+  "static/chunks/node_modules_10f8adf8._.js"
+],
+    source: "dynamic"
+});
