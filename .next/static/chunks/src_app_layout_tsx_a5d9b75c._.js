759d6afc06f99994ac0298b18a2e60500d2dd0e6
surfkvasir6-ui <surfkvasir6@gmail.com>
2025-08-28 16:07:12 -0300

    Add .next/static/chunks/src_app_layout_tsx_a5d9b75c._.js 1756408031671

---
+++ .next/static/chunks/src_app_layout_tsx_a5d9b75c._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__85150599._.css",
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_contexts_f78731e4._.js"
+],
+    source: "dynamic"
+});
